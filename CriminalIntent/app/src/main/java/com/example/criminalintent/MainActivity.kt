932ed94907f402de1a383742f58ddb05d2f01b55
Kenny Ta <55076129+takenny@users.noreply.github.com>
2021-02-18 20:01:52 -0800

    adnroid lab 7
    lab 7 done

---
+++ CriminalIntent/app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val KTcurrentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (KTcurrentFragment == null) {
+            val KTfragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, KTfragment)
+                .commit()
+        }
+
+    }
+}
